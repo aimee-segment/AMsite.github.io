8be68c212bc1205a6cac52216ec97dcb9e22d3df
aimee-segment <38016467+aimee-segment@users.noreply.github.com>
2018-09-23 11:16:24 -0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,6 +20,24 @@
 			$body = $('body'),
 			$header = $('#header'),
 			$banner = $('#banner');
+		
+		// Video
+		var $refreshButton = $('#refresh');
+  		var $results = $('#css_result');
+		
+		function refresh(){
+    			var css = $('style.cp-pen-styles').text();
+    			$results.html(css);
+  		}
+
+  		refresh();
+  		$refreshButton.click(refresh);
+  
+  		// Select all the contents when clicked
+  		$results.click(function(){
+    			$(this).select();
+  		});
+  
 
 		// Disable animations/transitions until the page has loaded.
 			$body.addClass('is-loading');
